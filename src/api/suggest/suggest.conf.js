26e0914b10776f588112435da7d1e9e6da96f4e0
Morten Hofft <mortenhoefft@gmail.com>
2019-02-21 08:09:24 +0100

    suggest conf

--- src/api/suggest/suggest.conf.js
+++ src/api/suggest/suggest.conf.js
@@ -1,32 +1,32 @@
-import axios from '../axios';
-import qs from 'querystringify';
+import axios from "../axios";
+import qs from "querystringify";
 
 const config = {
   datasetKey: {
-    type: 'KEY',
-    endpoint: '//api.gbif.org/v1/dataset/suggest',
-    key: 'key',
-    title: 'title'
+    type: "KEY",
+    endpoint: "//api.gbif.org/v1/dataset/suggest",
+    key: "key",
+    title: "title"
   },
   taxon: {
-    type: 'KEY',
-    endpoint: '//api.gbif.org/v1/species/suggest',
-    key: 'key',
-    title: 'scientificName',
+    type: "KEY",
+    endpoint: "//api.gbif.org/v1/species/suggest",
+    key: "key",
+    title: "scientificName",
     description: function(item) {
-      let classification = '';
+      let classification = "";
       [
-        'kingdom',
-        'phylum',
-        'class',
-        'order',
-        'family',
-        'genus',
-        'species'
+        "kingdom",
+        "phylum",
+        "class",
+        "order",
+        "family",
+        "genus",
+        "species"
       ].forEach(function(rank) {
         if (item[rank]) {
-          if (classification !== '') {
-            classification += ' ❯ ';
+          if (classification !== "") {
+            classification += " ❯ ";
           }
           classification += item[rank];
         }
@@ -35,38 +35,55 @@ const config = {
     }
   },
   basisOfRecord: {
-    type: 'ENUM',
-    endpoint: '//api.gbif.org/v1/enumeration/basic/BasisOfRecord'
+    type: "ENUM",
+    endpoint: "//api.gbif.org/v1/enumeration/basic/BasisOfRecord"
   },
   issue: {
-    type: 'ENUM',
-    endpoint: '//api.gbif.org/v1/enumeration/basic/OccurrenceIssue'
+    type: "ENUM",
+    endpoint: "//api.gbif.org/v1/enumeration/basic/OccurrenceIssue"
   },
   countryCode: {
-    type: 'ENUM',
-    endpoint: '//api.gbif.org/v1/enumeration/basic/Country'
+    type: "ENUM",
+    endpoint: "//api.gbif.org/v1/enumeration/basic/Country"
   },
   institutionCode: {
-    type: 'STRING',
-    endpoint: '//api.gbif.org/v1/occurrence/search/institutionCode'
+    type: "STRING",
+    endpoint: "//api.gbif.org/v1/occurrence/search/institutionCode"
   }
 };
 
 // Shared suggest API: (q, limit, intl) => [{key, title, description}]
 let suggest = {};
 for (let key in config) {
-	const conf = config[key];
-	suggest[key] = (q, limit, intl) => axios.get(conf.endpoint) + qs.stringify({q, limit}, true).then(result => {
-		let description = conf.description || (() => undefined);
-		return {
-			key: result[conf.key || 'key'],
-			title: result[conf.title],
-			description: description(result)
-		}
-	});
+  const conf = config[key];
+  if (conf.type === "KEY") {
+    suggest[key] = (q, limit, intl) =>
+      axios.get(conf.endpoint + qs.stringify({ q, limit }, true)).then(response => {
+        const result = response.data;
+        let description = conf.description || (() => undefined);
+        return {
+          key: result[conf.key || "key"],
+          title: result[conf.title],
+          description: description(result)
+        };
+      });
+  }
+  if (conf.type === "ENUM") {
+    suggest[key] = (q, limit, intl) =>
+      axios.get(conf.endpoint + qs.stringify({ q, limit }, true)).then(response => {
+        return response.data
+          .filter(e =>
+            e
+              .toLowerCase()
+              .replace("_", " ")
+              .startsWith(searchText.toLowerCase())
+          )
+          .slice(0, 2);
+      });
+  }
 }
 
 export default {
-	config,
-	suggest
+  config,
+  suggest
 };
